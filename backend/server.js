dfe6b18642ff1a660045db819a8f8a6610111091
aloksingh <alokankushsingh@gmail.com>
2026-05-03 16:26:41 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,8 @@ const PORT = process.env.PORT || 5000;
 
 initDB()
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
+
       console.log(`🚀 Server running on port ${PORT}`);
     });
   })
